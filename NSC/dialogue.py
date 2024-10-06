c4bec339581eab13dacbc32c75758b31de3bbed5
R4inssss <peterbuinam@gmail.com>
2024-10-05 21:15:47 -0700

    Changed to 2 list of dictionaries

--- NSC/dialogue.py
+++ NSC/dialogue.py
@@ -6,32 +6,37 @@ from score import *
 from sprites import *
 
 
-class DialogueProgram():
+class DialogueProgram:
     def __init__(self):
         pass
 
 
-class MainText():
+class MainText:
     def __init__(self):
-        self.Atext = None
-        self.Btest = None
-        self.Ctext = None
-        self.Etext = None
+        self.A1text = None
+        self.B1text = None
+        self.C1text = None
+        self.E1text = None
+        self.A2text = None
+        self.B2text = None
+        self.C2text = None
+        self.E2text = None
 
     def AirDialogue(self):
-        self.Atext = [[{'A': 'Gas-powered transportation'}, {'B': 'Electric Transportation'},
-                  {'C': 'Human Powered Transportation'}, {'D': 'Water Transportation'}],
-                 [{'A': '', 'B': '', 'C': '', 'D': ''}]]
+        self.A1text = [{'A': 'Gas-powered transportation'}, {'B': 'Electric Transportation'},
+                  {'C': 'Human Powered Transportation'}, {'D': 'Water Transportation'}]
+        self.A2text = [{'A': '', 'B': '', 'C': '', 'D': ''}]
 
     def CarbonDialogue(self):
-        self.Btext = [[{}, {}, {}, {}],
-                 [{}, {}, {}, {}]]
+        self.B1text = [{}, {}, {}, {}]
+        self.B2text = [{}, {}, {}, {}]
 
     def SurfaceDialogue(self):
-        self.Ctext = [[{}, {}, {}, {}],
-                 [{}, {}, {}, {}]]
+        self.C1text = [{}, {}, {}, {}]
+        self.C2text[{}, {}, {}, {}]
+
 
     def WaterDialogue(self):
-        self.Etext = [[{}, {}, {}, {}],
-                 [{}, {}, {}, {}]]
+        self.E1text = [{}, {}, {}, {}]
+        self.E2text = [{}, {}, {}, {}]
 
