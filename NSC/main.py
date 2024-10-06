8fc53a2da66b1c3b696cd8a1bb9481bade02ba65
R4inssss <peterbuinam@gmail.com>
2024-10-06 07:20:54 -0700

    removed tilemap. Changing gameplay

--- NSC/main.py
+++ NSC/main.py
@@ -3,6 +3,9 @@ from sprites import *
 from config import *
 from protocols import *
 import sys
+from dialogue import MainText
+from dialogue import *
+
 
 
 class Game():
@@ -12,27 +15,17 @@ class Game():
         self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont('Arial', 30)
         self.running = True
+
         # Map Calls
         self.bg = pygame.image.load(r"./assets/bg.png")
 
-    def createTilemap(self):
-        for i, row in enumerate(tilemap):
-            for j, column in enumerate(row):
-                if column == "B":
-                    Block(self, j, i)
-                if column == "P":
-                    Player(self, j, i)
 
-            print(i, row)
 
     def new(self):
         # New game starts
         self.playing = True
-        self.all_sprites = pygame.sprite.LayeredUpdates()  # Object that contains all of our sprites && updates
-        self.blocks = pygame.sprite.LayeredUpdates()  # walls
-        #self.enemies = pygame.sprite.LayeredUpdates()               # enemies
-        #self.attacks = pygame.sprite.LayeredUpdates()               # attack animation
-        self.createTilemap()
+        self.all_sprites = pygame.sprite.LayeredUpdates()            # Object that contains all of our sprites && updates
+        self.blocks = pygame.sprite.LayeredUpdates()                 # walls
 
     def events(self):
         # Game loop events
